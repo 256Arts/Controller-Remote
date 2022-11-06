95e7801491f37ffea8620171f8d3a9317775fdab
256ArtsDeveloper <256arts@jaydenirwin.com>
2022-11-05 23:59:08 -0400

    Initial Commit

---
+++ Controller Remote/Controller_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Controller_RemoteApp.swift
+//  Controller Remote
+//
+//  Created by Jayden Irwin on 2022-11-05.
+//
+
+import SwiftUI
+
+@main
+struct Controller_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
